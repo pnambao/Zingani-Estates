83d0faadc712c4be43f5c9095e91bd1955852a7c
Panje Nambao <81390842+pnambao@users.noreply.github.com>
2024-09-29 14:52:46 -0500

    creating the route for stripe payments and setting up server

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const Stripe = require("stripe");
+const cors = require("cors"); 
+
 //initializing express and stripe 
 const app = express();
+const stripe = Stripe("sk_test_51Q390LIVlokKHLiOBMWfBQlaFe36TlER25jBqR3oMH3msHwdk9oz7Ki0dUfNDFrZweSsEotbHTAvpEGsilKLAA7V00l0iWiDuY");
 
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//APT route to create payment intent
+app.post('/api/create-payment-intent',async (req, res) => {
+    const {amount} =req.body;
+
+    try{
+        const paymentintent= await stripe.paymentIntents.create({
+            amount: amount * 100,
+            currency:'kwacha',
+        });
+
+        res.json({clientSecret:paymentintent.client_secret});
+    } catch (error){
+        res.status(500).json({error: "Payment failed", details: error.message});
+    }
+
+});
+
+//Starting the server
 let pay = document.getElementById("payment");

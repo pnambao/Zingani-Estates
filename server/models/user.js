9e8202ea97baec866f8bc5408faf692ad28761c5
Panje Nambao <81390842+pnambao@users.noreply.github.com>
2024-11-12 23:27:37 -0600

    more schema stuff

--- server/models/user.js
+++ server/models/user.js
@@ -7,3 +7,15 @@ const userSchema = new mongoose.Schema({
     email:{type:String, required: true, unique: true},
     role: {type:String, enum: ["tenant", "landlord"], required: true},
 }, {timestamps: true});
+
+//Encrypt password before saving
+userSchema.pre('save', async function (next) {
+if(!this.isModified('password')) return next();
+this.password = await bcrypt.hash(this.password, 10);
+next();    
+});
+
+//method for pw comaprison before login
+userSchema.methods.comparePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
